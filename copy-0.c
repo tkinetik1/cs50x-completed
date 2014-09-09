71ea2ce5f49d88bb86afb12996dd34adb72e5e23
Tyson Frederick <tkinetik1@gmail.com>
2014-09-08 21:31:30 -0700

    Create copy-0.c

---
+++ copy-0.c
@@ -0,0 +1,31 @@
+//completed 9/7/14 through cs50x: week 4 (5).
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+
+//missing 1 header file for toupper
+
+int main(void)
+{
+    printf("Say something: ");
+    string s = GetString();
+    if (s == NULL)
+    {
+        return 1;
+    }
+    
+    string t = s;
+    
+    printf("Capitalizing copy...\n");
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+    
+    printf("Original:   %s\n", s);
+    printf("Copy:       %s\n", t);
+}
